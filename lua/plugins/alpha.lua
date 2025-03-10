ac8bc0cba79663f5c44ee37ebe77c5d3e01fdac3
rhysEdison <rhys@edisoninteractive.com>
2025-03-09 20:07:13 -0600

    chore: alpha.lua configuration working

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -9,29 +9,40 @@ return {
     local time = os.date '%H:%M'
     local date = os.date '%a %d %b'
     local v = vim.version()
-    local version = ' v' .. v.major .. '.' .. v.minor .. '.' .. v.patch
+    local version = 'v' .. v.major .. '.' .. v.minor .. '.' .. v.patch
 
     -- Set header
     dashboard.section.header.val = {
-      '       ███╗░░██╗██╗░░░██╗██╗███╗░░░███╗',
-      '       ████╗░██║██║░░░██║██║████╗░████║',
-      '       ██╔██╗██║╚██╗░██╔╝██║██╔████╔██║',
-      '       ██║╚████║░╚████╔╝░██║██║╚██╔╝██║',
-      '       ██║░╚███║░░╚██╔╝░░██║██║░╚═╝░██║',
-      '       ╚═╝░░╚══╝░░░╚═╝░░░╚═╝╚═╝░░░░░╚═╝',
-      '',
-      '                 █▀█ █▄░█  █▀▄▀█ █▄█            ',
-      '                 █▄█ █░▀█  █░▀░█ ░█░            ',
-      '',
-      '       ░██╗░░░░░░░██╗░█████╗░██╗░░░██╗',
-      '       ░██║░░██╗░░██║██╔══██╗╚██╗░██╔╝',
-      '       ░╚██╗████╗██╔╝███████║░╚████╔╝░',
-      '       ░░████╔═████║░██╔══██║░░╚██╔╝░░',
-      '       ░░╚██╔╝░╚██╔╝░██║░░██║░░░██║░░░',
-      '       ░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝░░░╚═╝░░░',
+      '                       .-.              ',
+      '                      |_:_|             ',
+      '                     /(_Y_)\\            ',
+      '.                   ( \\/M\\/ )           ',
+      " '.               _.'-/'-'\\-'._         ",
+      "   ':           _/.--'[[[[]'--.\\_       ",
+      "     ':        /_'  : |::\"| :  '.\\      ",
+      "       ':     //   ./ |oUU| \\.'  :\\     ",
+      "         ':  _:'..' \\_|___|_/ :   :|    ",
+      "           ':.  .'  |_[___]_|  :.':\\    ",
+      '            [::\\ |  :  | |  :   ; : \\   ',
+      "             '-'   \\/'.| |.' \\  .;.' |  ",
+      "             |\\_    \\  '-'   :       |  ",
+      '             |  \\    \\ .:    :   |   |  ',
+      "             |   \\    | '.   :    \\  |  ",
+      '             /       \\   :. .;       |  ',
+      '            /     |   |  :__/     :  \\\\ ',
+      '           |  |   |    \\:   | \\   |   ||',
+      '          /    \\  : :  |:   /  |__|   /|',
+      "          |     : : :_/_|  /'._\\  '--|_\\",
+      "          /___.-/_|-'   \\  \\            ",
     }
 
-    dashboard.section.buttons.val = {}
+    dashboard.section.buttons.val = {
+      dashboard.button('n', '   New file', ':ene <BAR> startinsert <CR>'),
+      dashboard.button('f', '󰮗   Find file', ':cd $HOME | Telescope find_files<CR>'),
+      dashboard.button('r', '   Recent', ':Telescope oldfiles<CR>'),
+      dashboard.button('R', '󱘞   Ripgrep', ':Telescope live_grep<CR>'),
+      dashboard.button('q', '󰗼   Quit', ':qa<CR>'),
+    }
 
     function centerText(text, width)
       local totalPadding = width - #text
@@ -41,10 +52,6 @@ return {
     end
 
     dashboard.section.footer.val = {
-      centerText('Kaizoku Ou Ni Ore Wa Naru', 50),
-      ' ',
-      -- centerText("NvimOnMy_Way❤️", 50),
-      -- " ",
       centerText(date, 50),
       centerText(time, 50),
       centerText(version, 50),
